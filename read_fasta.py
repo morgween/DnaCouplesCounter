5629e90b52dd824ad5af549fdb18a7096c1a024a
Max <105385179+morgween@users.noreply.github.com>
2023-08-05 20:49:07 +0300

    Changes in results saving

--- read_fasta.py
+++ read_fasta.py
@@ -13,16 +13,16 @@ class Sequencer:
             print("File wasn't found, try again")
             sys.exit(1)
 
-    def read_sequences(self, file_path:str) -> dict[str, int]:
-        self.results: dict[str,int] = {}
+    def read_sequences(self, file_path: str) -> dict[str, int]:
+        self.results: dict[str, int] = {}
         with open(file_path, 'r') as file:
             for line in file:
-                if line.startswith('>'): #no need to do anything
+                if line.startswith('>'):  # no need to do anything
                     continue
                 else:
                     for i in range(len(line)-2):
                         couple = line[i:i+2]
-                        if(len(couple) < 2):
+                        if (len(couple) < 2):
                             continue
                         if couple in self.results:
                             self.results[couple] += 1
@@ -30,7 +30,8 @@ class Sequencer:
                             self.results[couple] = 1
 
     def create_histogram(self) -> None:
-        fig, ax = plt.subplots(num=None, figsize=(16, 12), dpi=80, facecolor='w', edgecolor='k')
+        fig, ax = plt.subplots(num=None, figsize=(
+            16, 12), dpi=80, facecolor='w', edgecolor='k')
 
         # Get the maximum and minimum values from the results dictionary
         max_value = max(self.results.values())
@@ -38,9 +39,9 @@ class Sequencer:
 
         # Create a colormap with three colors: yellow, green, and red
         colors = [
-            '#ABC270', #green min
-            '#77B7D7', #blue others
-            '#FEC868'  #yellow max
+            '#ABC270',  # green min
+            '#77B7D7',  # blue others
+            '#FEC868'  # yellow max
         ]
 
         # Plot the bars and set the colors based on the values
@@ -54,26 +55,26 @@ class Sequencer:
                 color_index = 1
 
             ax.bar(couple, value, color=colors[color_index])
-        
+
         mean_value = np.mean(list(self.results.values()))
         ax.axhline(y=mean_value, color='r', linestyle='--', label='Mean')
-        
-        green_patch = mpatches.Patch(color=colors[0], label='Minimum of apperances')
-        yellow_patch = mpatches.Patch(color=colors[2], label='Maximum of apperances')
+
+        green_patch = mpatches.Patch(
+            color=colors[0], label='Minimum of apperances')
+        yellow_patch = mpatches.Patch(
+            color=colors[2], label='Maximum of apperances')
         mean_line = mpatches.Patch(color='red', linestyle='--', label='Mean')
-        ax.legend(handles = [green_patch, yellow_patch, mean_line])
+        ax.legend(handles=[green_patch, yellow_patch, mean_line])
 
         script_dir = os.path.dirname(os.path.realpath(__file__))
         new_file_path = os.path.join(script_dir, 'results')
         if not os.path.exists(new_file_path):
-            os.makedirs(new_file_path)  
+            os.makedirs(new_file_path)
+        os.chdir(new_file_path)
+        plt.savefig('histogram.png')
 
-        plt.savefig(new_file_path+'histogram.png')
-
-        
 
 if __name__ == "__main__":
     solution = Sequencer()
     solution.read_sequences(solution.file_path)
     solution.create_histogram()
-
